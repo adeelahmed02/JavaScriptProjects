bb1fdf293f7bf19e518598ac6727e77d93ae6470
adeelahmed02 <64714274+adeelahmed02@users.noreply.github.com>
2020-08-25 05:16:02 -0400

    create js code required check form
    create js code required check form

---
+++ p01/script.js
@@ -0,0 +1,55 @@
+//Document Object Model
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// All Functions
+// Function to show error 
+function showError(input,massage) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = massage;
+}
+
+// Function to show success
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+
+
+// This is an Event listener for the form on 
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    if(username.value ===''){
+        showError(username,'Username is Required')
+    } else{
+        showSuccess(username);
+    }
+    if(email.value ===''){
+        showError(email,'Email is Required')
+    } else{
+        showSuccess(email);
+    }
+    if(password.value ===''){
+        showError(password,'Password is Required')
+    } else{
+        showSuccess(password);
+    }
+    if(password2.value ===''){
+        showError(password2,'Confirm Password is Required')
+    } else{
+        showSuccess(password2);
+    }
+ 
+})
+
+
+
+
+
