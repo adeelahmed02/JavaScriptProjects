0d072f8b00efedcf1cc676eaa26f290dc94d665e
adeelahmed02 <64714274+adeelahmed02@users.noreply.github.com>
2020-08-25 08:56:40 -0400

    Update script.js
    form limitation restriction applied

--- p01/script.js
+++ p01/script.js
@@ -22,12 +22,15 @@ function showSuccess(input){
 }
 
 // function to check if email is valied
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())){
+        showSuccess(input);
+    } else {
+        showError(input,`${getFieldId(input)} is not valid`);
+    }
+
 }
-
-
 
 // function to check if required fileds have data
 function checkRequired(inputArray){
@@ -54,6 +57,16 @@ function checkLength(input, min, max){
         showSuccess(input);
     }
     
+}
+
+// function to check if password are match
+function checkPasswordMatch(input1,input2){
+    if (input1.value !== input2.value){
+        showError(input2,"Password don't match");
+    }
+
+}
+
 
 // functoion to get the id of the input filed proper Case 
 
@@ -68,6 +81,8 @@ form.addEventListener('submit',function(e){
     checkRequired([username,email,password,password2]);
     checkLength(username,3,10);
     checkLength(password,6,30);
+    checkEmail(email);
+    checkPasswordMatch(password,password2);
 
 })
 
